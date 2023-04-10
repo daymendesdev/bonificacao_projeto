233a5898c11c015cceef35ca488be00f181e2201
Dayanne Mendes <daymendesdev@gmail.com>
2023-04-10 16:43:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ import pandas as pd
 
 lista_meses = ['janeiro','fevereiro','março','abril','maio','junho']
 
-tabela_vendas = pd.read_excel('janeiro.xlsx')
-print(tabela_vendas)
+for mes in lista_meses:
+    tabela_vendas = pd.read_excel(f'{mes}.xlsx')
+    if(tabela_vendas['Vendas'] > 55000).any():
+        vendedor = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendedor'].values[0]
+        vendas = tabela_vendas.loc[tabela_vendas['Vendas'] > 55000, 'Vendas'].values[0]
+        print(f'No mês {mes} alguém bateu a meta. Vendedor:{vendedor}, Vendas{vendas}')
+
